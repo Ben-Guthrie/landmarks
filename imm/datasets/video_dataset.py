6c6cb97da3ff664ed548f0cb123dcde489395525
Ben Guthrie <b.f.guthrie@soton.ac.uk>
2019-03-11 14:09:48 +0000

    Small additions

--- imm/datasets/video_dataset.py
+++ imm/datasets/video_dataset.py
@@ -13,15 +13,15 @@ def load_dataset(data_root, subset):
     images_set = []
 
     # image, future image, train/val/test
-    with open(osp.join(data_root, 'subsets', 'list_training_test.txt'), 'r') as f:
-        lines = f.readlines()
+    with open(osp.join(data_root, 'subsets', 'data_subsets.txt'), 'r') as f:
+        lines = f.readlines()[1:]
         for i, line in enumerate(lines):
             data = line.split()
             image_files.append(data[0])
-            future_image_files[i].append(data[1])
-            images_set[i].append(int(data[2]))
+            future_image_files.append(data[1])
+            images_set.append(int(data[2]))
 
-    assert image_files[0] =='00000.png'
+    assert image_files[0] =='000000.png'
 
     # Make sure no images are repeated
     assert len(image_files) == len(set(image_files))
@@ -37,6 +37,7 @@ def load_dataset(data_root, subset):
         raise ValueError(
             'subset = %s for video dataset not recognized.' % subset)
 
+    images_set = np.array(images_set)
     image_files = np.array(image_files)
     future_image_files = np.array(future_image_files)
     images = image_files[images_set == label]
@@ -47,58 +48,61 @@ def load_dataset(data_root, subset):
 
 class VideoDataset(ImagePairDataset):
 
-  def __init__(self, data_dir, subset, max_samples=None, order_stream=False, image_size=[128,128], name='VideoDataset'):
+    def __init__(self, data_dir, subset, max_samples=None, order_stream=False, image_size=[128,128], name='VideoDataset'):
 
-    super(VideoDataset, self).__init__(
-        data_dir, subset, image_size=image_size, jittering=None, name=name)
+        super(VideoDataset, self).__init__(
+            data_dir, subset, image_size=image_size, jittering=None, name=name)
 
-    self._image_dir, self._images, self._future_images = load_dataset(
-        self._data_dir, self._subset)
+        self._image_dir, self._images, self._future_images = load_dataset(
+            self._data_dir, self._subset)
 
-    self._max_samples = max_samples
-    self._order_stream = order_stream
-
-
-  def _get_sample_dtype(self):
-    d =  {'image': tf.string,
-          'future_image': tf.string}
-    return d
+        self._max_samples = max_samples
+        self._order_stream = order_stream
 
 
-  def _get_sample_shape(self):
-    d = {'image': None,
-         'future_image': None}
-    return d
+    def _get_sample_dtype(self):
+        d =  {'image': tf.string, 'future_image': tf.string}
+        return d
 
 
-  def _get_image(self, idx):
-    image = osp.join(self._image_dir, self._images[idx])
-    future_image = osp.join(self._image_dir, self._future_images[idx])
-
-    inputs = {'image': image, 'future_image': future_image}
-    return inputs
+    def _get_sample_shape(self):
+        d = {'image': None,
+             'future_image': None}
+        return d
 
 
-  def _get_random_image(self):
-    idx = np.random.randint(len(self._images))
-    return self._get_image(idx)
+    def _get_image(self, idx):
+        image = osp.join(self._image_dir, self._images[idx])
+        future_image = osp.join(self._image_dir, self._future_images[idx])
+
+        inputs = {'image': image, 'future_image': future_image}
+        return inputs
 
 
-  def _get_ordered_stream(self):
-    for i in range(len(self._images)):
-      yield self._get_image(i)
+    def _get_random_image(self):
+        idx = np.random.randint(len(self._images))
+        return self._get_image(idx)
 
 
-  def sample_image_pair(self):
-    f_sample = self._get_random_image
-    if self._order_stream:
-      g = self._get_ordered_stream()
-      f_sample = lambda: next(g)
-    max_samples = float('inf')
-    if self._max_samples is not None:
-      max_samples = self._max_samples
-    i_samp = 0
-    while i_samp < max_samples:
-      yield f_sample()
-      if self._max_samples is not None:
-          i_samp += 1
+    def _get_ordered_stream(self):
+        for i in range(len(self._images)):
+            yield self._get_image(i)
+
+
+    def sample_image_pair(self):
+        f_sample = self._get_random_image
+        if self._order_stream:
+            g = self._get_ordered_stream()
+            f_sample = lambda: next(g)
+        max_samples = float('inf')
+        if self._max_samples is not None:
+            max_samples = self._max_samples
+        i_samp = 0
+        while i_samp < max_samples:
+            yield f_sample()
+            if self._max_samples is not None:
+                i_samp += 1
+
+
+    def num_samples(self):
+        raise NotImplementedError()
